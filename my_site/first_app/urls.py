5de2c9a26b68669e438732b342a525857472d42c
satyakideshpande <satyaki747@gmail.com>
2024-05-09 05:29:26 +0530

    Connecting a view to a template

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('<int:num_page>',views.num_page_view),
-    path('<str:topic>/',views.news_view,name='topic-page')
+    path('',views.simple_view)
 ]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -3,27 +3,5 @@ from django.http.response import HttpResponse,Http404,HttpResponseNotFound,HttpR
 from django.urls import reverse
 
 # Create your views here.
-
-#Creating a dictionary that stores all the routes
-
-articles = {
-    'sports': 'Sports View',
-    'finance': 'Finance View',
-    'politics': 'Political View'
-}
-
-
-def news_view(request,topic):
-    try:
-        result = articles[topic]
-        return HttpResponse(articles[topic])
-    except:
-        #return Http404("404 Generic Response")
-        result = 'Page not found'
-        return HttpResponseNotFound(result)
-
-def num_page_view(request,num_page):
-    topics_list = list(articles.keys())
-    topic =  topics_list[num_page]
-
-    return HttpResponseRedirect(reverse('topic-page',args=[topic]))
+def simple_view(request):
+    return render(request, 'first_app/example.html')
