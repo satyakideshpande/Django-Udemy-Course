4d350070b96ffb40e7b20f7bfd8b4f49876ee19d
satyakideshpande <satyaki747@gmail.com>
2024-05-09 05:12:16 +0530

    Reverse URLs and URL Names

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('<topic>/',views.news_view),
-    path('<int:num1>/<int:num2>',views.add_view)
+    path('<int:num_page>',views.num_page_view),
+    path('<str:topic>/',views.news_view,name='topic-page')
 ]
