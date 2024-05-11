835a755cb9044fe2e38642655df3d2774e8f6509
satyakideshpande <satyaki747@gmail.com>
2024-05-11 10:00:35 +0530

    Refactored code - Added connection between T&V

--- my_site/first_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse,Http404,HttpResponseNotFound,HttpResponseRedirect
-from django.urls import reverse
-
-# Create your views here.
-def simple_view(request):
-    return render(request, 'first_app/example.html')
